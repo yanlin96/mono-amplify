e6a88f59b344cf001a8cea74d25b12228c29494c
yanlin96 <yanlin96.gong@gmail.com>
2022-06-27 11:17:00 +1000

    next

--- next-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ next-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nextapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
